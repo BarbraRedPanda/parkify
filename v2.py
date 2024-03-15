289fd776c2e755fb47d6fef14112d2661df311e4
Parsa Khairollahi <parsak9000@gmail.com>
2024-03-14 23:22:35 -0400

    Added metadata functionality

--- v2.py
+++ v2.py
@@ -8,11 +8,30 @@ import time
 #import numpy as np
 import geopy.distance
 import requests
+import json
 
 from PIL import Image
 from picamera import PiCamera
 
 
+gpsd.connect() # Connects to satellites
+
+# gets API token from .env file
+load_dotenv()
+TOKEN = os.getenv('API_TOKEN')
+
+
+# Adds folder for images
+os.makedirs("responses", exist_ok=True)
+subfolders = ['images', 'data']
+
+# Iterate over the subfolders and create them if they don't already exist
+for subfolder in subfolders:
+    subfolder_path = os.path.join('responses', subfolder)
+    os.makedirs(subfolder_path, exist_ok=True)
+
+# implements the Raspberry Pi camera 
+# returns image stream 
 def getImageStream():
     camera = PiCamera()
     image_stream = io.BytesIO()
@@ -28,21 +47,35 @@ def getResponse(image_stream):
         headers={'Authorization': f'Token {TOKEN}'}
     )
     print(response)
-    return response
+    return json.loads(response)
+
+# Returns an image stream with plates
+def addMetadata(image_stream, result):
+    # Parses information from result
+    plate = result.get('plate')
+    newData = { 'confidence': result.get('score'),
+                'vehicle': result.get('vehicle', {}).get('type')
+                }
+
+    img = Image.open(image_stream)                          
+    currentData = json.loads(img.info.get('metadata', {}))  # gets current metadata
+    currentData[plate] = newData                            # appends newly parsed info into metadata
+    img.info['metadata'] = json.dumps(currentData)          # n.b. the key for the data is the plate
+    
+    # returns as image stream
+    newStream = io.BytesIO()
+    img.save(newStream, format='JPEG')
+    return newStream
         
-def svImg(image, filename):
-    with open(f'image_{filename}', 'wb') as f:
-            f.write(image)
+# Saves image stream in ./responses/images directory
+def saveImage(image, filename):
+    with open(f"./responses/images/image_{filename}", 'wb') as f:
+            f.write(image.getvalue())
     print(f"Saved image as image_{filename}")
-
-gpsd.connect() # Connects to satellites
-
-# gets API token
-load_dotenv()
-TOKEN = os.getenv('API_TOKEN')
 
 i = 0
 
+# Takes a picture every 3 seconds if user has moved 
 coordsPrev = (0.0, 0.0)
 while(True):
     time.sleep(3)
@@ -50,12 +83,21 @@ while(True):
     packet = gpsd.get_current();
     coords = (packet.lat, packet.lon)
 
+    # Reiterates loop until GPS has moved significantly 
     if (geopy.distance.geodesic(coordsPrev, coords).km < 0.00001) :
         continue
 
     coordsPrev = coords
-    i= i+1
-    imageStream = getImageStream()
-    #svImg(imageStream.getvalue(), i)
-    getResponse(imageStream)
 
+    imageStream = getImageStream()
+
+    api_response = getResponse(imageStream)
+
+    # Reiterates loop if no plate is found 
+    if not api_response.get('results'):
+         continue
+    
+    i += 1
+    for result in api_response['results']:
+        saveImage(addMetadata(imageStream, result))
+    
